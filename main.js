47ea33d1fbe3d88cf805af2cf1cfbe2c4052fdf5
ElViejoMarto <117958908+ElViejoMarto@users.noreply.github.com>
2023-03-25 10:07:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,132 @@
+
+
+window.addEventListener("keydown", mi_keydown);
+
+
+function mi_keydown(e) {
+    keyPressed = e.keyCode;
+    if (keyPressed == "38") {
+        console.log("flecha arriba si funciona");
+        //up();
+    }
+    if (keyPressed == "40") {
+        console.log("flecha abajo si funciona");
+       // down();
+    }
+    if (keyPressed == "39") {
+        console.log("flecha derecha si funciona");
+        //right();
+    }
+    if (keyPressed == "37") {
+        console.log("flecha izquierda si funciona");
+       // left();
+    }
+    if (keyPressed == "65") {
+        console.log("tecla a si funciona");
+    }
+    if (keyPressed == "66") {
+        console.log("tecla b si funciona");
+    }
+    if (keyPressed == "67") {
+        console.log("tecla c si funciona");
+    }
+    if (keyPressed == "68") {
+        console.log("tecla d si funciona");
+    }
+    if (keyPressed == "69") {
+        console.log("tecla e si funciona");
+    }
+    if (keyPressed == "70") {
+        console.log("tecla f si funciona");
+    }
+    if (keyPressed == "71") {
+        console.log("tecla g si funciona");
+    }
+    if (keyPressed == "72") {
+        console.log("tecla h si funciona");
+    }
+    if (keyPressed == "73") {
+        console.log("tecla i si funciona");
+    }
+    if (keyPressed == "74") {
+        console.log("tecla j si funciona");
+    }
+    if (keyPressed == "75") {
+        console.log("tecla k si funciona");
+    }
+    if (keyPressed == "76") {
+        console.log("tecla l si funciona");
+    }
+    if (keyPressed == "77") {
+        console.log("tecla m si funciona");
+    }
+    if (keyPressed == "78") {
+        console.log("tecla n si funciona");
+    }
+    if (keyPressed == "79") {
+        console.log("tecla o si funciona");
+    }
+    if (keyPressed == "80") {
+        console.log("tecla p si funciona");
+    }
+    if (keyPressed == "81") {
+        console.log("tecla q si funciona");
+    }
+    if (keyPressed == "82") {
+        console.log("tecla r si funciona");
+    }
+    if (keyPressed == "83") {
+        console.log("tecla s si funciona");
+    }
+    if (keyPressed == "84") {
+        console.log("tecla t si funciona");
+    }
+    if (keyPressed == "85") {
+        console.log("tecla u si funciona");
+    }
+    if (keyPressed == "86") {
+        console.log("tecla v si funciona");
+    }
+    if (keyPressed == "87") {
+        console.log("tecla w si funciona");
+    }
+    if (keyPressed == "88") {
+        console.log("tecla x si funciona");
+    }
+    if (keyPressed == "89") {
+        console.log("tecla y si funciona");
+    }
+    if (keyPressed == "90") {
+        console.log("tecla z si funciona");
+    }
+}
+
+//function up() {
+    //if (rover_y >= 0) {
+        //rover_y = rover_y - 20;
+        //uploadBackground();
+        //uploadRover();
+   // }
+//}
+ //function down() {
+    //if (rover_y <= 500) {
+       // rover_y = rover_y + 20;
+        //uploadBackground();
+        //uploadRover();
+    //}
+//}
+ //function right() {
+   // if (rover_x <= 700) {
+       // rover_x = rover_x + 20;
+       // uploadBackground();
+        //uploadRover();
+   // }
+//}
+ //}
+ //function left() {
+     //if (rover_x >= 0) {
+        // rover_x = rover_x - 20;
+        // uploadBackground();
+         //uploadRover();
+   //  }
+ //}
